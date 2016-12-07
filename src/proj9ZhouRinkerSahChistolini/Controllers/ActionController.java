c9892d435bffb9a3196702c3b6117fe2a4bab12d
Edo-kun <edwardzwz@gmail.com>
2016-12-07 00:44:06 -0500

    project rename
    EZ POINTS LETS GO

---
+++ src/proj9ZhouRinkerSahChistolini/Controllers/ActionController.java
@@ -0,0 +1,206 @@
+/**
+ * File: ActionController.java
+ * @author Victoria Chistolini
+ * @author Edward (osan) Zhou
+ * @author Alex Rinker
+ * @author Vivek Sah
+ * Class: CS361
+ * Project: 5
+ * Date: Nov 1, 2016
+ */
+package proj9ZhouRinkerSahChistolini.Controllers;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import proj9ZhouRinkerSahChistolini.Controllers.Actions.Actionable;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
+/**
+ * Crontrols the actions preformed in the composition pane
+ */
+public class ActionController {
+
+    /** Cached list to check if unsaved actions*/
+    private List<Actionable> saveList;
+
+    /** List to hold undone action states */
+    private ObservableList<Actionable> undoList;
+    /** List to hold redone action states */
+    private ObservableList<Actionable> redoList;
+
+    /**
+     * Initialized ActionController with empty stacks
+     */
+    public ActionController() {
+        List<Actionable> uList = new ArrayList<>();
+        List<Actionable> rList= new ArrayList<>();
+        this.undoList = FXCollections.observableList(uList);
+        this.redoList = FXCollections.observableList(rList);
+        this.saveList = new ArrayList<>();
+    }
+
+    /**
+     * Reverts the state of the application to previous state
+     */
+    public void undo() {
+        if (!this.undoList.isEmpty()) {
+            Actionable prevState = peekUndo();
+            prevState.unDoIt();
+            //remove after doing it to attain newest state for bindings
+            popUndo();
+            this.redoList.add(prevState);
+        }
+    }
+
+    /**
+     * Redoes the changes that have been undone
+     */
+    public void redo(){
+        if (!this.redoList.isEmpty()) {
+            Actionable undidState = peekRedo();
+            undidState.reDoIt();
+            popRedo();
+            this.undoList.add(undidState);
+        }
+    }
+
+    /**
+     * Each new action must clear the redo stack,
+     * then adds the action to the undo stack
+     */
+    public void addAction(Actionable action) {
+        this.redoList.clear();
+        this.undoList.add(action);
+    }
+
+    /**
+     * returns a ListProperty of the actions in the undoList
+     * @return ListProperty undoActions
+     */
+    public ListProperty<Actionable> getUndoActionsProperty() {
+        //Bind obp to children and list to obp
+        ObjectProperty<ObservableList<Actionable>> obp = new SimpleObjectProperty();
+        obp.setValue(this.undoList);
+        ListProperty<Actionable> undoActions = new SimpleListProperty();
+        undoActions.bind(obp);
+        return undoActions;
+    }
+
+    /**
+     * returns a ListProperty of the actions in the redoList
+     * @return ListProperty redoActions
+     */
+    public ListProperty<Actionable> getRedoActionsProperty() {
+        //Bind obp to children and list to obp
+        ObjectProperty<ObservableList<Actionable>> obp = new SimpleObjectProperty();
+        obp.setValue(this.redoList);
+        ListProperty<Actionable> redoActions = new SimpleListProperty();
+        redoActions.bind(obp);
+        return redoActions;
+    }
+
+    /**
+     * returns a BooleanBinding representing whether or not the undoList is empty
+     * @return BooleanBinding
+     */
+    public BooleanBinding isUndoEmpty() {
+        BooleanBinding emptyUndoBinding = Bindings.createBooleanBinding(() ->
+                this.undoList.size() < 1,
+                this.undoList
+        );
+        return emptyUndoBinding;
+    }
+
+    /**
+     * returns a BooleanBinding representing whether or not the redoList is empty
+     * @return BooleanBinding
+     */
+    public BooleanBinding isRedoEmpty() {
+        BooleanBinding emptyRedoBinding = Bindings.createBooleanBinding(() ->
+                this.redoList.size() < 1,
+                this.redoList
+        );
+        return emptyRedoBinding;
+    }
+
+    /**
+     * returns the top element of the undoList
+     * throws an emptyStackException
+     * @return undoList[0]
+     */
+    public Actionable peekUndo() throws EmptyStackException {
+        if(undoList.size() > 0) { return undoList.get(undoList.size() - 1); }
+        throw new EmptyStackException();
+    }
+
+    /**
+     * returns the top element of the redoList
+     * @return redoList[0]
+     */
+    public Actionable peekRedo() throws EmptyStackException {
+        if(redoList.size() > 0) { return redoList.get(redoList.size() - 1); }
+        throw new EmptyStackException();
+    }
+
+    /**
+     * removes and returns the top element of the undoList
+     * @return undoList[0]
+     */
+    public Actionable popUndo() throws EmptyStackException {
+        if(undoList.size() > 0) { return undoList.remove(undoList.size() - 1); }
+        throw new EmptyStackException();
+    }
+
+    /**
+     * removes and returns the top element of the redoList
+     * @return redoList[0]
+     */
+    public Actionable popRedo() throws EmptyStackException {
+        if(redoList.size() > 0) { return redoList.remove(redoList.size() - 1); }
+        throw new EmptyStackException();
+    }
+
+    /**
+     * clears the undo and redo lists
+     */
+    public void clearLists() {
+        this.undoList.clear();
+        this.redoList.clear();
+    }
+
+    /** update the saveList */
+    public void saveList(){
+        this.saveList = new ArrayList<>();
+        undoList.forEach(n-> this.saveList.add(n));
+    }
+
+    /**
+     * Checks if state is saved
+     * @return boolean state
+     */
+    public boolean isSaved(){
+        if (this.saveList.size() == this.undoList.size()){
+            for (int i=0; i<this.saveList.size();i++){
+                if (!this.saveList.get(i).equals(this.undoList.get(i))){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
+
---
+++ src/proj9ZhouRinkerSahChistolini/Models/Composition.java
@@ -0,0 +1,154 @@
+/**
+ * File: Composition.java
+ * @author Victoria Chistolini
+ * @author Tiffany Lam
+ * @author Joseph Malionek
+ * @author Vivek Sah
+ * Class: CS361
+ * Project: 4
+ * Date: October 11, 2016
+ */
+
+package proj9ZhouRinkerSahChistolini.Models;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Collection;
+
+/**
+ * The central logic for creating and playing back a composition.
+ */
+public class Composition {
+    /** The volume at which all the notes will be played */
+    public static final int VOLUME = 127;
+    /** The track on which this composition will be played */
+    public static final int TRACK_INDEX = 0;
+
+    /** The midiplayer on which this composition will be played */
+    private MidiPlayer player;
+
+    /** The notes to build the composition */
+    private HashSet<Playable> notes;
+
+    /** Creates a new Composition object with a midiplayer which has
+     * 60 beats per minute and 100 ticks per beat*/
+    public Composition(){
+        player = new MidiPlayer(100,60);
+        notes = new HashSet<>();
+    }
+    /**
+     * This will add a note to this composition
+     * given a set of note parameters into a midi event
+     * @param startTick the tick at which this note will be played
+     * @param duration how long this note will be played
+     * @param pitch the pitch of the note
+     * @param instrument the instrument with which this note will be played
+     *
+     */
+    public void addNote(int startTick, int duration, int pitch, Instrument instrument, int volume) {
+        this.player.addNote(pitch, volume, startTick,
+                duration, instrument.getChannel(), this.TRACK_INDEX,
+                instrument.getValue());
+    }
+
+    /**
+     * Adds a note to this composition
+     * @param note the note which will be added to the composition
+     */
+    public void addNote(Playable note){
+        addNote(note, 0);
+    }
+
+    public void addNote(Playable note, int offset){
+        if (note instanceof Note) {
+            Note temp = (Note) note;
+            this.addNote(temp.getStartTick() - offset, temp.getDuration(),
+                    temp.getPitch(), temp.getInstrument(), temp.volumeProperty().intValue());
+        }
+        else if(note instanceof Gesture){
+            ((Gesture) note).getChildren().forEach(n -> this.addNote(n, offset));
+        }
+    }
+
+    /**
+     * Appends a playable to the note field
+     * removes children of gestures from top of collection
+     * @param note the note to be added to the field
+     */
+    public void appendPlayable(Playable note){
+        if (note instanceof Gesture){
+            this.notes.removeAll(((Gesture) note).getChildren());
+        }
+        this.notes.add(note);
+
+
+    }
+
+    /**
+     * Removes a note from the notes field
+     * @param note the note to be removed
+     */
+    public void removeNote(Note note){
+        this.notes.remove(note);
+    }
+
+    /**
+     * Constructs a composition from the notes collection
+     */
+    public void buildSong(){
+        for(Playable note: notes){
+            this.addNote(note);
+        }
+    }
+
+    /**
+     * Constructs a sectional composition from a collection of notes
+     * @param notes
+     */
+    public void buildSong(Collection<Playable> notes){
+        double minX = notes.stream().min(Comparator.comparing(Playable::getX)).get().getX();
+        for(Playable note: notes){
+            this.addNote(note, (int) minX);
+        }
+    }
+
+    /**
+     * plays the current composition
+     */
+    public void play() {
+        this.player.play();
+    }
+
+    /**
+     * stops the current composition and clears the midi player
+     */
+    public void stop() {
+        this.player.clear();
+        this.player.stop();
+    }
+
+    /**
+     * getter for notes
+     * @return
+     */
+    public HashSet<Playable> getNotes() {
+        return notes;
+    }
+
+
+    /**
+     * gets the selected notes from Compostion
+     * @return collection of note objects which are selected
+     */
+    public Collection<Playable> getSelectedCompositionNotes() {
+        HashSet<Playable> selectedNotes = new HashSet<>();
+
+        for (Playable note : notes) {
+            if (note.selectedProperty().getValue()) {
+                selectedNotes.add(note);
+            }
+        }
+
+        return selectedNotes;
+    }
+}
